45e961145f8341578f02d7fc6c486f700d134982
rongrong <1399811201@qq.com>
2017-05-31 21:47:55 +0800

    添加静态工厂方法创建bean实例 工程方法创建bean实例

--- src/main/java/com/spring/bean/factory/CarFactoryBean.java
+++ src/main/java/com/spring/bean/factory/CarFactoryBean.java
@@ -2,23 +2,22 @@ package com.spring.bean.factory;
 
 import java.util.HashMap;
 import java.util.Map;
-
- 
 /**
- * 静态工厂方法创建bean实例
+ * 	通过实例工厂方法创建bean
  * @author Administrator
  *
  */
-public class StaticFactory {
+public class CarFactoryBean {
+
+	Map<String,Car>cars=new HashMap<String,Car>();
 	
-	static Map<String,Car> cars=new HashMap<String, Car>();
-	static{
+	public CarFactoryBean() {
+		// TODO Auto-generated constructor stub
 		cars.put("ford", new Car("ford", 300000));
 		cars.put("audi", new Car("audi",500000));
 	}
 	
-	public static Car getCar(String brand){
+	public  Car getCar(String brand){
 		return cars.get(brand);
 	}
-	
 }
---
+++ src/main/java/com/spring/bean/factory/Main.java
@@ -0,0 +1,15 @@
+package com.spring.bean.factory;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-bean-factorybean.xml");
+		Car car = (Car) ctx.getBean("staticFactory");
+		System.out.println(car);
+		car = (Car) ctx.getBean("car");
+		System.out.println(car);
+	}
+}
